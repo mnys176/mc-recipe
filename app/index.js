7a8b14e626f7ddb3c8be7f01afb03c7d4471ee82
Mike Nystoriak <nystoriakm@gmail.com>
2021-08-27 11:44:18 -0400

    Added server entrypoint and configured ExpressJS.

---
+++ app/index.js
@@ -0,0 +1,19 @@
+/********************************************************
+ * Title:       index.js                                *
+ * Author:      Mike Nystoriak (nystoriakm@gmail.com)   *
+ * Created:     08/27/2021                              *
+ * Description:                                         *
+ *     An API that communicates with a MongoDB instance *
+ *     to manage a recipe book for meals discovered and *
+ *     created throughout the years.                    *
+ ********************************************************/
+
+const express = require('express')
+const dotenv = require('dotenv').config().parsed
+
+const app = express()
+const port = process.env.PORT || dotenv.PORT || 8080
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.listen(port, () => console.log(`Running on port: ${port}`))

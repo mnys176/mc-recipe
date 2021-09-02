2003440f202acfde542b947db474888fa96317d9
Mike Nystoriak <nystoriakm@gmail.com>
2021-09-02 14:37:40 -0400

    Upgraded Mongoose.

--- app/util/mongo.js
+++ app/util/mongo.js
@@ -12,10 +12,7 @@ const mongoose = require('mongoose')
 
 const connectToMongoDB = async () => {
     try {
-        const mongoConn = await mongoose.connect(process.env.MONGODB_URI, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true
-        })
+        const mongoConn = await mongoose.connect(process.env.MONGODB_URI)
         const mongoDomain = `${mongoConn.connection.host}:${mongoConn.connection.port}`
         console.log(`Connection to MongoDB at ${mongoDomain} succeeded!`)
     } catch (err) {

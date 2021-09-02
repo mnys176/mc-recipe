bb1bb0e1799657ccc16b59eb853c1191d6efd995
Mike Nystoriak <nystoriakm@gmail.com>
2021-09-02 17:18:28 -0400

    Moved `enum.js` to exist within `quantify.js`.

--- app/util/quantify.js
+++ app/util/quantify.js
@@ -9,12 +9,34 @@
  *     converting from one unit to another.           *
  ******************************************************/
 
-const {
-    time,
-    volume,
-    mass,
-    misc
-} = require('./enum')
+const time = { DAYS: 'd', HOURS: 'h', MINUTES: 'm' }
+
+const volume = {
+    // imperial
+    GALLONS: 'gal',
+    QUARTS: 'qt',
+    PINTS: 'pt',
+    CUPS: 'c',
+    TABLESPOONS: 'tbsp',
+    TEASPOONS: 'tsp',
+
+    // metric
+    LITERS: 'L',
+    MILLILITERS: 'mL'
+}
+
+const mass = {
+    // imperial
+    OUNCES: 'oz',
+    POUNDS: 'lb',
+
+    // metric
+    KILOGRAMS: 'kg',
+    GRAMS: 'g',
+    MILLIGRAMS: 'mg'
+}
+
+const misc = { PIECES: 'pieces', TO_TASTE: 'to taste' }
 
 /**
  * Enforces instantiation policy on abstractions.

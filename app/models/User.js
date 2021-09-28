05665417fa698c164431a893ff10fdcc599325e9
Mike Nystoriak <nystoriakm@gmail.com>
2021-09-28 18:06:20 -0400

    Established a baseline for a user model with Mongoose.

---
+++ app/models/User.js
@@ -0,0 +1,40 @@
+/******************************************************
+ * Title:       User.js                               *
+ * Author:      Mike Nystoriak (nystoriakm@gmail.com) *
+ * Created:     09/28/2021                            *
+ * Description: Models a user using Mongoose schemas. *
+ ******************************************************/
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const nameSchema = new Schema({
+    first: { type: String, required: true, match: /^[A-Z'](\.|[A-z\-']*)$/ },
+    middle: { type: String, default: '', match: /^[A-Z']?(\.|[A-z\-']*)$/ },
+    last: { type: String, required: true, match: /^[A-Z'](\.|[A-z\-']*)$/ }
+})
+
+const userSchema = new Schema({
+    name: { type: nameSchema, required: true },
+    username: {
+        type: String,
+        required: true,
+        match: /^\w+$/,
+        minLength: 6
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 60,
+        maxLength: 60
+    },
+    email: {
+        type: String,
+        required: true,
+        match: /^[\w\.]+@\w+(\.\w+)+$/
+    },
+    media: { type: String, default: '' },
+    registered: { type: Number, default: Date.now() }
+})
+
+module.exports = mongoose.model('User', userSchema)

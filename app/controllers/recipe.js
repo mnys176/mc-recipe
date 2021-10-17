36e427815f3b38a2cb25e5b4c1b816e76b76c178
Mike Nystoriak <nystoriakm@gmail.com>
2021-10-17 15:11:38 -0400

    Added JSDoc comments.

--- app/controllers/recipe.js
+++ app/controllers/recipe.js
@@ -7,31 +7,80 @@
 
 const { recipeService } = require('../services')
 
+/**
+ * Gets all recipes in the database.
+ * 
+ * @author Mike Nystoriak (nystoriakm@gmail.com)
+ * 
+ * @param {object} req Request object from Express.
+ * @param {object} res Response object from Express.
+ */
 const getAllRecipes = async (req, res) => {
     const { status, data } = await recipeService.fetch()
     return res.status(status).json(data)
 }
 
+/**
+ * Gets a single recipe from the database by its ID.
+ * 
+ * @author Mike Nystoriak (nystoriakm@gmail.com)
+ * 
+ * @param {object} req Request object from Express.
+ * @param {object} res Response object from Express.
+ */
 const getRecipeById = async (req, res) => {
     const { status, data } = await recipeService.fetchById(req.params.id)
     return res.status(status).json(data)
 }
 
+/**
+ * Creates a recipe in the database.
+ * 
+ * @author Mike Nystoriak (nystoriakm@gmail.com)
+ * 
+ * @param {object} req Request object from Express.
+ * @param {object} res Response object from Express.
+ */
 const postRecipe = async (req, res) => {
     const { status, data } = await recipeService.create(req.body)
     return res.status(status).json(data)
 }
 
+/**
+ * Updates a recipe in the database.
+ * 
+ * @author Mike Nystoriak (nystoriakm@gmail.com)
+ * 
+ * @param {object} req Request object from Express.
+ * @param {object} res Response object from Express.
+ */
 const putRecipe = async (req, res) => {
     const { status, data } = await recipeService.change(req.params.id, req.body)
     return res.status(status).json(data)
 }
 
+/**
+ * Deletes a recipe in the database.
+ * 
+ * @author Mike Nystoriak (nystoriakm@gmail.com)
+ * 
+ * @param {object} req Request object from Express.
+ * @param {object} res Response object from Express.
+ */
 const deleteRecipe = async (req, res) => {
     const { status, data } = await recipeService.discard(req.params.id)
     return res.status(status).json(data)
 }
 
+/**
+ * Gets an image file from the database that is linked to
+ * a recipe by its unique filename.
+ * 
+ * @author Mike Nystoriak (nystoriakm@gmail.com)
+ * 
+ * @param {object} req Request object from Express.
+ * @param {object} res Response object from Express.
+ */
 const getRecipeMedia = async (req, res) => {
     const { id, filename } = req.params
     const { status, data } = await recipeService.fetchMedia(id, filename)
@@ -44,16 +93,41 @@ const getRecipeMedia = async (req, res) => {
               .send(file)
 }
 
+/**
+ * Attaches an image to a recipe (only works when no media is
+ * currently linked).
+ * 
+ * @author Mike Nystoriak (nystoriakm@gmail.com)
+ * 
+ * @param {object} req Request object from Express.
+ * @param {object} res Response object from Express.
+ */
 const postRecipeMedia = async (req, res) => {
     const { status, data } = await recipeService.setMedia(req.params.id, req.files)
     return res.status(status).json(data)
 }
 
+/**
+ * Changes the image associated with a recipe,
+ * 
+ * @author Mike Nystoriak (nystoriakm@gmail.com)
+ * 
+ * @param {object} req Request object from Express.
+ * @param {object} res Response object from Express.
+ */
 const putRecipeMedia = async (req, res) => {
     const { status, data } = await recipeService.resetMedia(req.params.id, req.files)
     return res.status(status).json(data)
 }
 
+/**
+ * Removes the image associated with a recipe,
+ * 
+ * @author Mike Nystoriak (nystoriakm@gmail.com)
+ * 
+ * @param {object} req Request object from Express.
+ * @param {object} res Response object from Express.
+ */
 const deleteRecipeMedia = async (req, res) => {
     const { status, data } = await recipeService.unsetMedia(req.params.id)
     return res.status(status).json(data)
--- app/controllers/user.js
+++ app/controllers/user.js
@@ -7,31 +7,80 @@
 
 const { userService } = require('../services')
 
+/**
+ * Gets all users in the database.
+ * 
+ * @author Mike Nystoriak (nystoriakm@gmail.com)
+ * 
+ * @param {object} req Request object from Express.
+ * @param {object} res Response object from Express.
+ */
 const getAllUsers = async (req, res) => {
     const { status, data } = await userService.fetch()
     return res.status(status).json(data)
 }
 
+/**
+ * Gets a single user from the database by its ID.
+ * 
+ * @author Mike Nystoriak (nystoriakm@gmail.com)
+ * 
+ * @param {object} req Request object from Express.
+ * @param {object} res Response object from Express.
+ */
 const getUserById = async (req, res) => {
     const { status, data } = await userService.fetchById(req.params.id)
     return res.status(status).json(data)
 }
 
+/**
+ * Creates a user in the database.
+ * 
+ * @author Mike Nystoriak (nystoriakm@gmail.com)
+ * 
+ * @param {object} req Request object from Express.
+ * @param {object} res Response object from Express.
+ */
 const postUser = async (req, res) => {
     const { status, data } = await userService.create(req.body)
     return res.status(status).json(data)
 }
 
+/**
+ * Updates a user in the database.
+ * 
+ * @author Mike Nystoriak (nystoriakm@gmail.com)
+ * 
+ * @param {object} req Request object from Express.
+ * @param {object} res Response object from Express.
+ */
 const putUser = async (req, res) => {
     const { status, data } = await userService.change(req.params.id, req.body)
     return res.status(status).json(data)
 }
 
+/**
+ * Deletes a user in the database.
+ * 
+ * @author Mike Nystoriak (nystoriakm@gmail.com)
+ * 
+ * @param {object} req Request object from Express.
+ * @param {object} res Response object from Express.
+ */
 const deleteUser = async (req, res) => {
     const { status, data } = await userService.discard(req.params.id)
     return res.status(status).json(data)
 }
 
+/**
+ * Gets an image file from the database that is linked to
+ * a user by its unique filename.
+ * 
+ * @author Mike Nystoriak (nystoriakm@gmail.com)
+ * 
+ * @param {object} req Request object from Express.
+ * @param {object} res Response object from Express.
+ */
 const getUserMedia = async (req, res) => {
     const { id, filename } = req.params
     const { status, data } = await userService.fetchMedia(id, filename)
@@ -44,16 +93,41 @@ const getUserMedia = async (req, res) => {
               .send(file)
 }
 
+/**
+ * Attaches an image to a user (only works when no media is
+ * currently linked).
+ * 
+ * @author Mike Nystoriak (nystoriakm@gmail.com)
+ * 
+ * @param {object} req Request object from Express.
+ * @param {object} res Response object from Express.
+ */
 const postUserMedia = async (req, res) => {
     const { status, data } = await userService.setMedia(req.params.id, req.files)
     return res.status(status).json(data)
 }
 
+/**
+ * Changes the image associated with a user,
+ * 
+ * @author Mike Nystoriak (nystoriakm@gmail.com)
+ * 
+ * @param {object} req Request object from Express.
+ * @param {object} res Response object from Express.
+ */
 const putUserMedia = async (req, res) => {
     const { status, data } = await userService.resetMedia(req.params.id, req.files)
     return res.status(status).json(data)
 }
 
+/**
+ * Removes the image associated with a user,
+ * 
+ * @author Mike Nystoriak (nystoriakm@gmail.com)
+ * 
+ * @param {object} req Request object from Express.
+ * @param {object} res Response object from Express.
+ */
 const deleteUserMedia = async (req, res) => {
     const { status, data } = await userService.unsetMedia(req.params.id)
     return res.status(status).json(data)

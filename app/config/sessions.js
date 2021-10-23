9d93eb814454275ec3979215b1b71667181c7345
Mike Nystoriak <nystoriakm@gmail.com>
2021-10-23 10:05:23 -0400

    Updated session management configuration.

--- app/config/sessions.js
+++ app/config/sessions.js
@@ -6,11 +6,18 @@ const store = new MongoDBStore({
     uri: process.env.MONGODB_URI ?? 'mongodb://localhost:27017/test',
     collection: 'sessions'
 })
+
+// configure middleware
 const sessionMiddleware = session({
-    secret: 'youuuuu',
-    store,
+    secret: process.env.COOKIE_SECRET ?? 'i-am-insecure-and-should-not-be-used',
+
+    // cookies expire after one hour
+    cookie: { maxAge: 3600000 },
+
+    unset: 'destroy',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store,
 })
 
 module.exports = sessionMiddleware
